2355aa6de6117ce5232b70730db1beded576492d
Anas Asimi <anas2020cr7@gmail.com>
2023-06-15 18:18:27 +0100

    add conditionally rendring related to state hook and create controll functions (next,back)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,65 @@
+import { useState } from "react";
+import Progress from "./components/Progress";
+import Info from "./components/Info";
+import Plan from "./components/Plan";
+import Add_ons from "./components/Add_ons";
+import Summary from "./components/Summary";
+import Thank from "./components/thank";
+
 export default function App() {
+  /*
+  we are representing pregess by number 
+  while first page is 1
+  and last one (thanks page) is 5
+   */
+  let [progress, setProgress] = useState(1);
+  function next() {
+    if (progress == 5) return;
+    setProgress((progress) => progress + 1);
+  }
+  function back() {
+    if (progress == 1) return;
+    setProgress((progress) => progress - 1);
+  }
   return (
     <>
-      <main className="w-100 flex h-screen items-center justify-center">
-        <h1 className="text-3xl font-bold text-teal-100">
-          Hello world!
-        </h1>
+      <main className="w-100 flex h-full gap-8 rounded-md p-8">
+        <Progress progress={progress} />
+        {progress == 1 ? (
+          <Info
+            className="flex flex-grow items-center justify-center"
+            next={next}
+            back={back}
+          />
+        ) : null}
+        {progress == 2 ? (
+          <Plan
+            className="flex flex-grow items-center justify-center"
+            next={next}
+            back={back}
+          />
+        ) : null}
+        {progress == 3 ? (
+          <Add_ons
+            className="flex flex-grow items-center justify-center"
+            next={next}
+            back={back}
+          />
+        ) : null}
+        {progress == 4 ? (
+          <Summary
+            className="flex flex-grow items-center justify-center"
+            next={next}
+            back={back}
+          />
+        ) : null}
+        {progress == 5 ? (
+          <Thank
+            className="flex flex-grow items-center justify-center"
+            next={next}
+            back={back}
+          />
+        ) : null}
       </main>
     </>
   );
