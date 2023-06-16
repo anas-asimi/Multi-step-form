051a51d31d91f5fc5a4995dd2fd938097b1bcba8
Anas Asimi <anas2020cr7@gmail.com>
2023-06-17 00:55:08 +0100

    keep working

--- src/App.jsx
+++ src/App.jsx
@@ -24,36 +24,22 @@ export default function App() {
   return (
     <main className="flex bg-white p-4">
       <Progress progress={progress} />
-      <div className="flex w-[600px] flex-col justify-between px-16 py-4">
-        {progress == 1 ? <Info /> : null}
-        {progress == 2 ? <Plan /> : null}
-        {progress == 3 ? <Add_ons /> : null}
-        {progress == 4 ? <Summary /> : null}
-        {progress == 5 ? <Thank /> : null}
-        <div className="controll flex w-full justify-between">
-          <button
-            className={
-              "select-none rounded-md px-8 py-2 capitalize " +
-              (progress == 1
-                ? "cursor-not-allowed bg-transparent text-gray-600"
-                : "bg-blue-600 text-white")
-            }
-            onClick={back}
-          >
-            go back
-          </button>
-          <button
-            className={
-              "select-none rounded-md px-8 py-2 capitalize " +
-              (progress == 5
-                ? "cursor-not-allowed bg-transparent text-gray-600"
-                : "bg-blue-600 text-white")
-            }
-            onClick={next}
-          >
-            next step
-          </button>
-        </div>
+      <div className="flex w-[800px] flex-col justify-between px-24 py-4">
+        {progress == 1 ? (
+          <Info progress={progress} back={back} next={next} />
+        ) : null}
+        {progress == 2 ? (
+          <Plan progress={progress} back={back} next={next} />
+        ) : null}
+        {progress == 3 ? (
+          <Add_ons progress={progress} back={back} next={next} />
+        ) : null}
+        {progress == 4 ? (
+          <Summary progress={progress} back={back} next={next} />
+        ) : null}
+        {progress == 5 ? (
+          <Thank progress={progress} back={back} next={next} />
+        ) : null}
       </div>
     </main>
   );
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,6 +1,81 @@
 /* eslint-disable react/prop-types */
-export default function Info() {
-  return <div className="">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore nemo quasi, ullam tempore hic doloribus ratione. Inventore excepturi tempore quibusdam. Libero sit dignissimos ipsum sunt perspiciatis quam ratione quisquam voluptatem.
-  </div>;
+export default function Info({ progress, next, back }) {
+  return (
+    <>
+      <div className="mt-8 h-full">
+        <h1 className="font-UbuntuBold text-3xl text-blue-700 mb-2">
+          Personnel info
+        </h1>
+        <p className="mb-8 text-gray-500">
+          Please provide your name, email address, and phone number.
+        </p>
+        <div className="input-wrapper mb-4">
+          <div className="input flex flex-col gap-1">
+            <label htmlFor="name" className="text-blue-700">
+              Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              className="h-12 rounded-md border-2 border-gray-300 px-2"
+              placeholder="eg. Stephen King"
+            />
+          </div>
+        </div>
+        <div className="input-wrapper mb-4">
+          <div className="input flex flex-col gap-1">
+            <label htmlFor="email" className="text-blue-700">
+              Email Address
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="h-12 rounded-md border-2 border-gray-300 px-2"
+              placeholder="eg. stephenking@lorem.com"
+            />
+          </div>
+        </div>
+        <div className="input-wrapper mb-4">
+          <div className="input flex flex-col gap-1">
+            <label htmlFor="phone" className="text-blue-700">
+              Phone Number
+            </label>
+            <input
+              type="text"
+              name="phone"
+              id="phone"
+              className="h-12 rounded-md border-2 border-gray-300 px-2"
+              placeholder="eg. +1 234 567 890"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="controll flex w-full">
+        <button
+          className={
+            "mr-auto select-none rounded-md px-8 py-2 capitalize " +
+            (progress == 1
+              ? "cursor-not-allowed bg-transparent text-gray-600"
+              : "bg-blue-600 text-white")
+          }
+          onClick={back}
+        >
+          go back
+        </button>
+        <button
+          className={
+            "ml-auto select-none rounded-md px-8 py-2 capitalize " +
+            (progress == 5
+              ? "cursor-not-allowed bg-transparent text-gray-600"
+              : "bg-blue-600 text-white")
+          }
+          onClick={next}
+        >
+          next step
+        </button>
+      </div>
+    </>
+  );
 }
