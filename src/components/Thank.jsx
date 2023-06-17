7a53271d3082b9f452c50d7315a676132cba4fc0
Anas Asimi <anas2020cr7@gmail.com>
2023-06-17 20:46:10 +0100

    complete Thank page

--- src/components/Thank.jsx
+++ src/components/Thank.jsx
@@ -1,7 +1,21 @@
 export default function Thank() {
   return (
     <>
-      <div className="mt-8 h-full">
+      <div className="flex gap-4 h-full w-full flex-col items-center justify-center">
+        <img
+          src="images/icon-thank-you.svg"
+          alt="thank you"
+          height="80"
+          width="80"
+        />
+        <h1 className="mt-4 font-UbuntuBold text-3xl text-blue-700">
+          Thank you
+        </h1>
+        <p className="mb-8 text-gray-500 text-center">
+          Thanks for confirming your subscription! We hope you have fun using
+          our platform. If you ever need support, please feel free to email us
+          at support@loremgaming.com.
+        </p>
         {/* put the html here */}
       </div>
     </>
