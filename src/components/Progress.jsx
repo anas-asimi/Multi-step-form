0e98a69252f0d408097a64fdb0830b8317e7e38d
Anas Asimi <anas2020cr7@gmail.com>
2023-06-18 16:01:23 +0100

    fix background image url

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -2,7 +2,7 @@ import Step from "./Step";
 
 export default function Progress({ progress }) {
   return (
-    <ul className="flex min-h-[568px] flex-col gap-10 rounded-lg bg-[url('images/bg-sidebar-desktop.svg')] bg-cover bg-no-repeat bg-bottom p-12 pr-20 text-white">
+    <ul className="flex min-h-[568px] flex-col gap-10 rounded-lg bg-[url('/images/bg-sidebar-desktop.svg')] bg-cover bg-no-repeat bg-bottom p-12 pr-20 text-white">
       <li>
         <Step number={1} title="your info" active={progress == 1} />
       </li>
